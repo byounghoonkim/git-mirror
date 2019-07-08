16e5d5f76a5e1f0b980016a910f99f99f4e666ab
shoebillk <blue021@gmail.com>
2019-07-08 18:12:37 +0900

    Add gitCloneHadler

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"strings"
 	"time"
 )
 
@@ -37,9 +38,29 @@ func main() {
 	}
 
 	// Run HTTP server to serve mirrors.
-	http.Handle("/", http.FileServer(http.Dir(cfg.BasePath)))
+	//http.Handle("/", http.FileServer(http.Dir(cfg.BasePath)))
+	http.Handle("/", gitCloneHandler(cfg, http.FileServer(http.Dir(cfg.BasePath))))
 	log.Printf("starting web server on %s", cfg.ListenAddr)
 	if err := http.ListenAndServe(cfg.ListenAddr, nil); err != nil {
 		log.Fatalf("failed to start server, %s", err)
 	}
+}
+
+func gitCloneHandler(cfg config, next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+
+		rpstr := r.URL.String()[1:]
+
+		if -1 < strings.Index(rpstr, ".git/info/") || strings.HasSuffix(rpstr, ".git") {
+			rpstr = rpstr[:strings.Index(rpstr, ".git")+4]
+			rp := repo{rpstr, "https://" + rpstr, duration{}}
+			if err := mirror(cfg, rp); err != nil {
+				log.Fatal("Clone Error", err, rp, r)
+			} else {
+				log.Print("Clone Success", rp)
+			}
+		}
+
+		next.ServeHTTP(w, r)
+	})
 }
